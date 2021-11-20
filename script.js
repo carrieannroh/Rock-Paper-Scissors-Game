c95c0fc0d5ad770b34c209a37da55c32f42faac9
Carrie Roberts <carriearoberts95@gmail.com>
2021-11-20 11:52:05 +0000

    Committing final solution

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //grab the buttons from the page
 
 const userButtons = document.querySelectorAll(".move-button")
+console.log(userButtons)
 //Get the computer choice and outcome elements from html
 
 const computerChoice = document.querySelector("#computer-choice")
@@ -11,16 +12,52 @@ const outcomeText = document.querySelector(".outcome-text")
 //Assign users choice to a variable
 
 const getUserChoice = (event) => {
-    const userChoice = event.target.innerText;
-    return "user choice: " + userChoice;
-}
+    return event.target.innerText;
+};
+
 //generate a move for the computer
+const getCompChoice = () => {
+    const choices = ["Rock", "Paper", "Scissors"];
+    const index = Math.floor(Math.random() * 3);
+    computerChoice.innerText = choices[index];
+    return choices[index];
+}
+
 //work out who won
+const calculateWinner = (event) => {
+    const userChoice = getUserChoice(event);
+    const compChoice = getCompChoice();
+//check if there is a tie
+if (userChoice === compChoice) {
+    return "It's a tie!"};
+
+if ((userChoice === "Paper" && compChoice === "Scissors") || (userChoice === "Rock" && compChoice === "Paper") || (userChoice === "Scissors" && compChoice === "Rock"))  {
+    return "Computer wins!"
+} else {
+    return "User Wins!"
+};
+
+// if (userChoice === "Rock" && compChoice === "Paper") {
+//     return "Computer wins!"
+// } else {
+//     return "User wins!"
+// };
+
+// if (userChoice === "Scissors" && compChoice === "Rock") {
+//     return "Computer wins!"
+// } else {
+//     return "User wins!"
+// };
+};
+
+
 //display the winner
+function updateDOM(event) {
+    outcomeText.textContent = calculateWinner(event)
+};
+    
 
 //function to add eventlistenr to element
-
-function updateDOM() {
-outcomeText.innerHTML = getUserChoice()};
-
-userButtons.forEach(element => {element.eventlistener("click", updateDOM)})
+userButtons.forEach((element) => {
+    element.addEventListener("click", updateDOM);
+});
